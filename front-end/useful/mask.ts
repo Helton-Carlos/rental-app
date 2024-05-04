43447ed01c6cb0f4b62f9f74aedf72b1793289fe
Helton Silva <helton.silva@melhorenvio.com>
2024-05-04 10:35:11 -0300

    fix: remove value default

--- front-end/useful/mask.ts
+++ front-end/useful/mask.ts
@@ -34,9 +34,7 @@ export function maskPJ(num: number | string) {
 
 export function maskMoney(value: number | string) {
   const number = typeof value === 'number' ? value.toString() : value;
-
-  if (!number) return '0,00';
-
+  
   const [integerPart, decimalPart] = number.split('.');
 
   const formattedIntegerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
