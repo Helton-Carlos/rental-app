debdbdff3376fe689d27d0eddd69dccdf0ac0da2
Helton Silva <helton.silva@melhorenvio.com>
2024-05-23 12:52:15 -0300

    remove: remove script unused

--- front-end/app.vue
+++ front-end/app.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <div class="flex flex-col min-h-screen">
     <NuxtPwaManifest />
--- front-end/pages/information/index.vue
+++ front-end/pages/information/index.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-</script>
-
 <template>
  <div>
   <h2 class="text-2xl mt-10 mx-5 font-bold md:my-2 md:text-3xl">
